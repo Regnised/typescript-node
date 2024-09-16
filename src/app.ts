283f3dd1d69d9257fa2e9bbf92c9c218caec3f1d
Vasyl Onopriienko <regnise@gmail.com>
2024-09-16 12:47:54 +0300

    add class-validator, class-transformer

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { json } from 'body-parser';
+import _ from './libs';
+import { Product } from './product.model';
+import 'reflect-metadata';
+import { plainToClass, plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
 
 import todoRoutes from './routes/todos';
 
@@ -11,5 +16,26 @@ app.use('/todos', todoRoutes);
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: err.message });
 });
+
+// CLASS validator
+const products = [
+  { title: '', price: 1 },
+  { title: '22222', price: 10 },
+];
+
+const loadedProducts = plainToInstance(Product, products);
+
+console.log(loadedProducts[0]);
+const p1 = new Product('A book', 123);
+for (const prod of loadedProducts) {
+  validate(prod).then((errors) => {
+    if (errors.length > 0) {
+      console.log(errors);
+    }
+  });
+  console.log(prod.getInformation());
+}
+
+// class validator
 
 app.listen(3000);
---
+++ src/libs.ts
@@ -0,0 +1,9 @@
+import _ from 'lodash';
+
+//
+console.log(_.shuffle([1, 2, 3]));
+
+declare var GLOBAL: any;
+console.log(GLOBAL);
+
+export default {};
